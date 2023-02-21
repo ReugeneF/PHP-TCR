4e25be7b8ed3b3a3c9e47c8d2fdda653112492f8
ReugeneF <reugene_business@hotmail.com>
2023-02-21 10:52:11 +0100

    oke

---
+++ Opdracht_P3_3/6_1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    session_start();
+    
+
+    if(isset['']){
+
+    }
+
+
+    echo "Deze pagina heb je al:" . $n . "keer bekenen" . "<br>";
+    echo "voordat je de internet browser hebt afgesloten";
+ 
+    ?>
+</body>
+</html>
